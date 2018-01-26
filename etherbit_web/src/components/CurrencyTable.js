d2049a96629c2b40979b203977b4684a0eb981e8
kunalsharma6996 <kunalsharma.ks13@gmail.com>
2018-01-26 16:28:14 +0530

    initial commit

---
+++ etherbit_web/src/components/CurrencyTable.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+
+class CurrencyTable extends Component {
+	render() {
+	    return (
+	    	<div>
+	    	<div class="container">
+	    	
+	    	</div>
+	    	</div>
+	    )
+	  }
+}
+
+
+export default CurrencyTable;
---
+++ etherbit_web/src/models/currency.js
@@ -0,0 +1,14 @@
+function Currency(currency) {
+    this.cap24hrChange = currency.cap24hrChange;
+    this.long = currency.long;
+    this.mktcap = currency.mktcap;
+    this.perc = currency.perc;
+    this.price = currency.price;
+    this.shapeshift = currency.shapeshift;
+    this.short = currency.short;
+    this.supply = currency.supply;
+    this.supplyusdVolume = currency.supplyusdVolume;
+    this.volume = currency.volume;
+    this.vwapData = currency.vwapData;
+    this.vwapDataBTC = currency.vwapDataBTC;
+}
